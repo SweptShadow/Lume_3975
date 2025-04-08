08711a138ad8e027d93bcd61c41371ced0a26f7c
Yujin Jeong <te0209494@gmail.com>
2025-04-07 17:38:17 -0700

    Created some files and folders for front end

---
+++ frontend_TS/src/routes/Routes.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
+import OotdPage from "../pages/OotdPage";
+import OotdPost from "../pages/OotdPostPage";
+import Discover from "../pages/DiscoverPage";
+import App from "../App";
+
+/**
+ * Define the application's routes.
+ */
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />
+      },
+      {
+        path: "/ootd",
+        element: <OotdPage />
+      },
+      {
+        path: "/ootd:id",
+        element: <OotdPost />
+      }
+    ],
+  },
+]);
