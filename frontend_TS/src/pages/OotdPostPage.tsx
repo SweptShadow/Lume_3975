1173f5aee0bb8844533ce09275d1dbbea2f9da90
Brian Diep <bdiep6@my.bcit.ca>
2025-04-09 00:15:07 -0700

    Setup ootd framework for accepting images

--- frontend_TS/src/pages/OotdPostPage.tsx
+++ frontend_TS/src/pages/OotdPostPage.tsx
@@ -4,8 +4,8 @@ import { OOTDService } from "../services/OOTDService";
 import { OOTDPost } from "../models/OOTDPost";
 
 /**
- * This renders for an individual post by id.
- *  e.g. /ootd/1, /ootd/2
+ * This renders an individual post by id.
+ * e.g. /ootd/1, /ootd/2
  */
 const OOTDPostPage = () =>
 {
@@ -31,13 +31,24 @@ const OOTDPostPage = () =>
     if (!post) return <div>Post not found.</div>;
 
     return (
-        <div className="ootd-post-page">
-            <h2>{ post.title }</h2>
-            {/* Currently using a mock image */}
-            <img src="/images/mockimage.jpeg" alt="OOTD" className="img-fluid" /> 
-             {/* { post.img } This should be  */}
+        <div className="ootd-container">
+            <div className="ootd-column justify-content-center">
+                <div className="post-column" >
+                    <div className="indie-card mb-4 mx-auto" style={ { maxWidth: '500px' } }>
+                        <h1 className="ootd-indie-title text-center w-100">{ post.title }</h1>
+                        <img
+                            // src={ `/storage/${ post.img }` }
+                            src="/images/mockimage.jpeg" // Mock image for now
+                            alt={ post.title }
+                            className="ootd-indie-img img-fluid mb-1"
+                        />
+                        <p className="ootd-indie-description">{ post.description }</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default OOTDPostPage;
+
